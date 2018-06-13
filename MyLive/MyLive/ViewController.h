979aba6be16c1e4bd76047f4f2ff458bf77618d3
lm <lmlm@lmdemac.local>
2018-06-13 01:29:18 -0700

    project init

---
+++ MyLive/MyLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLive
+//
+//  Created by lm on 18/6/13.
+//  Copyright © 2018年 lm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
